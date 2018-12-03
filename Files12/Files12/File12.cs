7d15194dd91f85805bf0227763cd7a1cfd6173d7
iJacob10 <06iylinjacob@gmail.com>
2018-12-03 23:14:40 +0530

    First commit file12 , file34

---
+++ Files12/Files12/File12.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1234
+{
+    class File12
+    {
+        static void Main(string[] args)
+        {
+            File12 f1 = new File12();
+            f1.printfun();
+
+            File2 f2 = new File2();
+            f2.printfun();
+
+            File3 f3 = new File3();
+            f3.printfun();
+
+            File4 f4 = new File4();
+            f4.printfun();
+            Console.ReadKey();
+        }
+        public void printfun()
+        {
+            int i = 1;
+            Console.WriteLine("Hello for file " + i);
+        }
+
+    }
+    class File2
+    {
+        public void printfun()
+        {
+            int i = 2;
+            Console.WriteLine("Hello for file " + i);
+        }
+    }
+}
+
---
+++ Files12/Files12/File34.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1234
+{
+    class File3
+    {
+        public void printfun()
+        {
+            int i = 3;
+            Console.WriteLine("Hello for file " + i);
+
+        }
+    }
+
+    class File4
+    {
+        public void printfun()
+        {
+            int i = 4;
+            Console.WriteLine("Hello for file " + i);
+        }
+    }
+}
